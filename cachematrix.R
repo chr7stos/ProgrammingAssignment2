b6d82fff6e2164dc07b9dc7cabb58f772150ab2a
Christos C. Y <christoscy@protonmail.ch>
2015-11-21 16:57:21 +0100

    solution
    solution to assignment 2, of coursera R-programming (Data Science
    specialisation)

--- cachematrix.R
+++ cachematrix.R
@@ -1,52 +1,34 @@
 ## makeCacheMatrix 
-## function taking matrix x
-## contains 4 other functions, like make makeVector
-## in this case, we need the inverse of a matrix, so our functions will be:
-## set, to set the matrix and get, to get the matri
-## setInv to set the inverse of the matrix, getInv to get it
-## the return values of these functions are finally stored in a list
+## this function creates a special matrix, which is an object that once its inverse can be computed by cacheSolve, it can cache it
+## with the help of the scoping rules in R, it can cache its inverse that it is set to it by cacheSolve
+
 
 makeCacheMatrix <- function(x = matrix()) {
 		invMat <- NULL
 		set <- function(y) {
-			x <<- y # sets x to a new vector y
-			invMat <<- NULL # initialises invMat to NULL
+			x <<- y 
+			invMat <<- NULL 
 		}
-		get <- function() x # returns the matrix x in input
-		setInv <- function(inverseMat) invMat <- inverseMat # sets the inverse matrix
+		get <- function() x 
+		setInv <- function(inverseMat) invMat <<- inverseMat 
 		getInv <- function () invMat
 		list(set = set, get = get, setInv = setInv, getInv = getInv)
-		#this special matrix, is a list  containing a function to set and get the matrix and then set and get the inverse of that matrix
+		
 }
 
-#1 Create the makeCacheMatrix, that is a function that gets an object x that is a matrix()
-#2 Initialise the invMat, that is the name of the inverse matrix that we will get afterwards
-#3 Make the function set, this function takes y as an argument...
+## cacheSolve
+## cacheSolve is a function, that computes the inverse of the special matrix, that is found in makeCacheMatrix 
+## if the inverse has been computed, then it gets the inverse from the cache, without re-computing
 
-#4 Make the function get, that is a function that just returns the x matrix in the input
-#5 Make setInv, that is a function that takes as argument the inverseMat and in its body, it assigns the inversMat to invMat
-#6 The getInv function returns the invMat
-#7 Create a list that has four values, that are equal to the function outputs
-
-
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { 
 	invMat <- x$getInv() #get the inverse of the matrix by looking into the list x gives out
 	if(!is.null(invMat)){
 		message("getting cached inverse")
-		return(invMat) # if is not null, it will get what was calculated above
-        ## Return a matrix that is the inverse of 'x'
+		return(invMat) 
 	}
+	# in the following, if the invMat is NULL, it will be computed 
 	data <- x$get() #if there is nothing there, it gets the matrix
-	invMat <- solve(data, ...) #it calculates the inverse matrix and assings it to inv
-	x$setInv(invMat) #sets the inv to the object x
-	invMat #returns the result of the solved matrix
+	invMat <- solve(data, ...) 
+	x$setInv(invMat) #sets the invMat to the object x as to get cached
+	invMat 
 }
-
-#1 After we give a matrix as input, the invMat will be NULL. In that case, the solve function will solve for the inverse of the matrix
-#2 The second time we will call the function cacheSolve though, it will look in the x object to find if there is invMat in there
-#3 If it finds it, it will just return it without recalculating it,
-
-
-
-
